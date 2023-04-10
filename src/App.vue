b90e142f26061a429431b4f88a498db2526e7610
EMcNugget <ethan.d.seys@gmail.com>
2023-04-10 17:24:05 -0500

    Added dividers

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,51 @@
-<script setup lang="ts"></script>
-
 <template>
   <div class="wrapper">
-    <h1>Clouds & Atmosphere</h1>
-    <div class="separator"></div>
-    <h1>Wind</h1>
-    <div class="separator"></div>
-    <h1>Temperature</h1>
+    <div class="left-pane">
+      <slot name="left-pane">
+        <div class="pane">
+          <h1>Clouds & Atmosphere</h1>
+        </div>
+      </slot>
+    </div>
+    <div class="divider" @mousedown="startResize"></div>
+    <div class="right-pane">
+      <slot name="right-pane">
+        <div class="pane">
+          <h1>Wind</h1>
+        </div>
+      </slot>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const startResize = (e: MouseEvent) => {
+  const divider = e.target as HTMLElement
+  const leftPane = divider.previousElementSibling as HTMLElement
+  const rightPane = divider.nextElementSibling as HTMLElement
+  const startX = e.clientX
+  const startWidth = leftPane.offsetWidth
+
+  const mouseMove = (e: MouseEvent) => {
+    const newWidth = startWidth + (e.clientX - startX)
+    leftPane.style.width = `${newWidth}px`
+    rightPane.style.width = `${window.innerWidth - newWidth}px`
+  }
+
+  const mouseUp = () => {
+    window.removeEventListener('mousemove', mouseMove)
+    window.removeEventListener('mouseup', mouseUp)
+  }
+
+  window.addEventListener('mousemove', mouseMove)
+  window.addEventListener('mouseup', mouseUp)
+
+  return () => {
+    window.removeEventListener('mousemove', mouseMove)
+    window.removeEventListener('mouseup', mouseUp)
+  }
+}
+</script>
 
 <style scoped>
 .separator {
@@ -21,5 +58,31 @@
   display: flex;
   justify-content: space-between;
   border-bottom: 1px solid #ccc;
+  height: 100%;
+  max-width: 100%;
+}
+
+.left-pane {
+  display: flex;
+  justify-content: center;
+  margin-right: 1rem;
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.right-pane {
+  display: flex;
+  justify-content: center;
+  margin-left: 1rem;
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.divider {
+  width: 5px;
+  cursor: ew-resize;
+  background-color: #ccc;
 }
 </style>
