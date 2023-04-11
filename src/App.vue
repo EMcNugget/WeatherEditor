48dc60405bc7ab7bd9fb156b10f3c77794d2375e
EMcNugget <ethan.d.seys@gmail.com>
2023-04-11 12:45:44 -0500

    Added yarn, redid styles

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,52 @@
+<script setup lang="ts"></script>
+
 <template>
-  <div class="wrapper">
+  <div class="main">
     <div class="left-pane">
-      <slot name="left-pane">
-        <div class="pane">
-          <h1>Clouds & Atmosphere</h1>
-        </div>
-      </slot>
+      <h3 class="title">Clouds & Atmosphere</h3>
+      <n-input-number class="forms" />
     </div>
-    <div class="divider" @mousedown="startResize"></div>
     <div class="right-pane">
-      <slot name="right-pane">
-        <div class="pane">
-          <h1>Wind</h1>
-        </div>
-      </slot>
+      <h3 class="title">Wind</h3>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-const startResize = (e: MouseEvent) => {
-  const divider = e.target as HTMLElement
-  const leftPane = divider.previousElementSibling as HTMLElement
-  const rightPane = divider.nextElementSibling as HTMLElement
-  const startX = e.clientX
-  const startWidth = leftPane.offsetWidth
-
-  const mouseMove = (e: MouseEvent) => {
-    const newWidth = startWidth + (e.clientX - startX)
-    leftPane.style.width = `${newWidth}px`
-    rightPane.style.width = `${window.innerWidth - newWidth}px`
-  }
-
-  const mouseUp = () => {
-    window.removeEventListener('mousemove', mouseMove)
-    window.removeEventListener('mouseup', mouseUp)
-  }
-
-  window.addEventListener('mousemove', mouseMove)
-  window.addEventListener('mouseup', mouseUp)
-
-  return () => {
-    window.removeEventListener('mousemove', mouseMove)
-    window.removeEventListener('mouseup', mouseUp)
-  }
-}
-</script>
+<script></script>
 
 <style scoped>
-.separator {
-  width: 0.5rem;
-  cursor: ew-resize;
-  background-color: #4f6c89;
-}
-
-.wrapper {
+.main {
   display: flex;
-  justify-content: space-between;
-  border-bottom: 1px solid #ccc;
-  height: 100%;
-  max-width: 100%;
+  flex-direction: row;
+  width: 100%;
 }
-
 .left-pane {
   display: flex;
   justify-content: center;
-  margin-right: 1rem;
+  flex-direction: column;
   width: 50%;
   height: 100%;
   overflow: hidden;
+  padding-left: 2.5rem;
+}
+
+.forms {
+  width: 50%;
+}
+
+.title {
+  border-bottom: white 1px solid;
+  margin-bottom: 3rem;
 }
 
 .right-pane {
   display: flex;
   justify-content: center;
-  margin-left: 1rem;
+  flex-direction: column;
+  padding-left: 2rem;
   width: 50%;
   height: 100%;
   overflow: hidden;
-}
-
-.divider {
-  width: 5px;
-  cursor: ew-resize;
-  background-color: #ccc;
+  padding-right: 2.5rem;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import naive from "naive-ui";
+
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(naive)
 
 app.mount('#app')
