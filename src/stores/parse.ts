65e03737a90839a5a62e01718b26abb3925ea8bc
EMcNugget <ethan.d.seys@gmail.com>
2023-04-13 09:18:08 -0500

    updated types

--- src/stores/parse.ts
+++ src/stores/parse.ts
@@ -1,10 +1,10 @@
 import { defineStore } from 'pinia'
-import type { TWeather } from './defs'
+import type { TWeather } from '@/libs/weather'
 
 export const weatherStore = defineStore('weather', {
   state: () => {
     return {
-      // reserved for future use, type is defined in the "Weather" interface
+      // reserved for future use, type is defined in the "TWeather" interface
     }
   }
 })
